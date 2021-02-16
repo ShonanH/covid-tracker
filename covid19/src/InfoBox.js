3146195317cff4309d3557389c5c37ab9ba7c642
ShonanH <shonanhendre@gmail.com>
2021-02-16 18:47:14 -0500

    general setup

---
+++ covid19/src/InfoBox.js
@@ -0,0 +1,25 @@
+import { Card, CardContent, Typography } from "@material-ui/core";
+import React from "react";
+
+function InfoBox({ title, cases, total }) {
+  return (
+    <div>
+      <Card className="infoBox">
+        <CardContent>
+          {/* // Title Covid cases */}
+          <Typography className="infoBox__title" color="textSecondary">
+            {title}
+          </Typography>
+          {/* // Number of cases */}
+          <h2 className="infoBox__cases">{cases}</h2>
+          {/* //Total */}
+          <Typography className="infoBox__total" color="textSecondary">
+            {total}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default InfoBox;
